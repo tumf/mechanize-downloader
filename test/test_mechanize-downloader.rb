2c55970235452ae0320cedf1b88302af2ec3559f
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-01-22 14:41:23 +0900

    Initial commit to mechanize-downloader.

---
+++ test/test_mechanize-downloader.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestMechanizeDownloader < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
